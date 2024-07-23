f84b615ac7c162ace4d5dc8cecf4042f64b99841
Diegoindio <dii.gomess@gmail.com>
2024-07-23 20:00:15 -0300

    selectFile

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -7,13 +7,13 @@ Cypress.Commands.add('fillSignupFormAndSubmit', (email, password) => {
     cy.contains('button', 'Signup').click()
     cy.get('#confirmationCode').should('be.visible')
     cy.mailosaurGetMessage(Cypress.env('MAILOSAUR_SERVER_ID'), {
-      sentTo: email
+        sentTo: email
     }).then(message => {
-      const confirmationCode = message.html.body.match(/\d{6}/)[0]
-      cy.get('#confirmationCode').type(`${confirmationCode}{enter}`)
-      cy.wait('@getNotes')
+        const confirmationCode = message.html.body.match(/\d{6}/)[0]
+        cy.get('#confirmationCode').type(`${confirmationCode}{enter}`)
+        cy.wait('@getNotes')
     })
-  })
+})
 
 
 // cypress/support/commands.js
@@ -23,21 +23,20 @@ Cypress.Commands.add('fillSignupFormAndSubmit', (email, password) => {
 Cypress.Commands.add('guiLogin', (
     username = Cypress.env('USER_EMAIL'),
     password = Cypress.env('USER_PASSWORD')
-  ) => { 
+) => {
     cy.intercept('GET', '**/notes').as('getNotes')
     cy.visit('/login')
     cy.get('#email').type(username)
     cy.get('#password').type(password, { log: false })
     cy.contains('button', 'Login').click()
-    cy.wait('@getNotes') 
+    cy.wait('@getNotes')
     cy.contains('h1', 'Your Notes').should('be.visible')
-  })
-  
-  Cypress.Commands.add('sessionLogin', (
+})
+
+Cypress.Commands.add('sessionLogin', (
     username = Cypress.env('USER_EMAIL'),
     password = Cypress.env('USER_PASSWORD')
-  ) => {
+) => {
     const login = () => cy.guiLogin(username, password)
     cy.session(username, login)
-  })
-  
+})
