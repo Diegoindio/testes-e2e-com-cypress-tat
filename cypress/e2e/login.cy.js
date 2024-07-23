2700b52583f1827e5d44274f1853156f9052eada
Diegoindio <dii.gomess@gmail.com>
2024-07-23 09:58:33 -0300

    Trabalhando com cy.session

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,9 +1,9 @@
 describe('Login', () => {
     it('successfully logs in', () => {
-      cy.intercept('GET', '**/notes').as('getNotes')
+      //cy.intercept('GET', '**/notes').as('getNotes')
   
-      cy.Login()
-      cy.wait('@getNotes')
+      cy.guiLogin()
+      //cy.wait('@getNotes')
   
       cy.contains('a', 'Create a new note').should('be.visible')
     })
