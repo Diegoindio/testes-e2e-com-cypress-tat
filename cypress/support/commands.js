3a68fa51b161b7a186715a942288ee6866e8ba87
Diegoindio <dii.gomess@gmail.com>
2024-07-23 19:50:01 -0300

    Update Login

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,13 +23,13 @@ Cypress.Commands.add('fillSignupFormAndSubmit', (email, password) => {
 Cypress.Commands.add('guiLogin', (
     username = Cypress.env('USER_EMAIL'),
     password = Cypress.env('USER_PASSWORD')
-  ) => {
+  ) => { 
     cy.intercept('GET', '**/notes').as('getNotes')
     cy.visit('/login')
     cy.get('#email').type(username)
     cy.get('#password').type(password, { log: false })
     cy.contains('button', 'Login').click()
-    cy.wait('@getNotes')
+    cy.wait('@getNotes') 
     cy.contains('h1', 'Your Notes').should('be.visible')
   })
   
