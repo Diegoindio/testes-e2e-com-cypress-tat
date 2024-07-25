0955514edd2118ed2f90e93b78cf9b1061f59851
Diegoindio <dii.gomess@gmail.com>
2024-07-25 20:56:26 -0300

    Update Github Action

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,11 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl:'https://notes-serverless-app.com',
+    baseUrl: 'https://notes-serverless-app.com',
+    env: {
+      viewportWidthBreakpoint: 768,
+    },
+
     chromeWebSecurity: false,
     setupNodeEvents(on, config) {
       // implement node event listeners here
